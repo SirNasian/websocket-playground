119e337ce17562764336f79737c91173fadf8653
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2022-02-04 01:07:56 +0800

    Implement `username` entry field

--- src/LoginScreen/LoginScreen.tsx
+++ src/LoginScreen/LoginScreen.tsx
@@ -1,8 +1,30 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, Paper, TextField } from "@mui/material";
 
-export const LoginScreen = () => {
-	return <Box>TODO: implement this!</Box>
+export const LoginScreen = ({
+	onLogin
+}: {
+	onLogin: (username: string) => void;
+}) => {
+	const [username, setUsername] = useState<string>("");
+	const loginDisabled = (username.trim() === '');
+	return (
+		<Paper>
+			<Box display="flex" p={2}>
+				<TextField
+					label="username"
+					value={username}
+					size="small"
+					sx={{marginRight:1}}
+					onChange={(event) => setUsername(event.target.value)}
+					onKeyPress={(event) => (!loginDisabled && (event.key === "Enter")) ? onLogin(username) : null}
+				/>
+				<Button variant="contained" disabled={loginDisabled} onClick={() => onLogin(username)}>
+					LOGIN
+				</Button>
+			</Box>
+		</Paper>
+	);
 };
 
 export default LoginScreen;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { Box } from "@mui/material";
 import { LoginScreen } from "./LoginScreen";
 
-type Screen = "login";
+type Screen = "login" | "chatroom";
 
 const App = () => {
 	const [ws, setWS] = useState<WebSocket>(undefined);
 	const [screen, setScreen] = useState<Screen>("login");
+	const [username, setUsername] = useState<string>("");
 
 	const connectWebSocket = () => {
 		const ws = new WebSocket("ws://localhost:42069/");
@@ -14,18 +16,24 @@ const App = () => {
 		setWS(ws);
 	};
 
+	const handleLogin = (username: string) => {
+		setUsername(username);
+		setScreen("chatroom");
+	};
+
 	const ScreenContent = () => {
 		switch (screen) {
-			case "login": return <LoginScreen />;
+			case "login": return <LoginScreen onLogin={handleLogin} />;
+			case "chatroom": return <Box><Box>TODO: implement this</Box><Box>Username: {username}</Box></Box>
 		}
 	};
 
 	useEffect(() => connectWebSocket(), []);
 
 	return (
-		<React.Fragment>
+		<Box display="flex" alignItems="center" justifyContent="center" height="100vh">
 			<ScreenContent />
-		</React.Fragment>
+		</Box>
 	);
 };
 
